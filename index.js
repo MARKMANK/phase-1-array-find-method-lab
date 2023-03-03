18d0ea970ca6dddcc2a4eff272721d216bfc580f
kevinmarkman <kevin.w.markman@gmail.com>
2023-03-03 09:51:27 -0600

    Adds superbowlWin function

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+// function superbowlWin(myArray){
+//     for(let element of myArray){
+//         if(element.result === "W")
+//             return element.year;
+//         }
+//     }
+
+
+function superbowlWin(myArray){
+    const isWinner = myArray.find((element) => {
+        return element.result === 'W'
+    })
+    return isWinner ? isWinner.year : undefined
+}
